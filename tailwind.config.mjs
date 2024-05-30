c876ce67f28afdb7fa9e2e4b94256914395e457a
Rose <rrortegaa@gmail.com>
2024-05-30 04:22:07 -0600

    :lipstick: tailwind config file added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        'french-rose': '#F35E8C',
+        'lavender-blush': '#FEF2F5',
+        'vista-white': '#FFFBFB',
+        'carnation': '#FF5865',
+        'green-leaf': '#417100',
+        'shark': '#262626',
+
+      },
+      fontFamily: {
+        serif: ['Kurale', ...defaultTheme.fontFamily.serif],
+        sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+      },
+    },
 	},
 	plugins: [],
 }
