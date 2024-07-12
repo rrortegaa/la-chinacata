69098b89a458b86cb114dc2ee669226118628130
Charles Silva <charlesipn@gmail.com>
2024-07-12 06:53:47 -0600

    contact form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly FORM_ACCESS_KEY: string; // w3forms access key
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
